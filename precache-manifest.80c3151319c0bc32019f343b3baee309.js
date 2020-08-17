d5cb965eb7ef19c732deaafaf214b659e08854c2
Louis Daza <louisdaza@hotmail.com>
2020-08-17 05:50:11 -0700

    Updates

---
+++ precache-manifest.80c3151319c0bc32019f343b3baee309.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a88fbbd194b52ea980d673423bc3423",
+    "url": "/bonjourDemo/index.html"
+  },
+  {
+    "revision": "c04a15857dafee6fb37c",
+    "url": "/bonjourDemo/static/css/main.4aadfecb.chunk.css"
+  },
+  {
+    "revision": "187b77d7d7368f34da38",
+    "url": "/bonjourDemo/static/js/2.b08dd489.chunk.js"
+  },
+  {
+    "revision": "a3765313e477a972575d5f52eb05cfbc",
+    "url": "/bonjourDemo/static/js/2.b08dd489.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c04a15857dafee6fb37c",
+    "url": "/bonjourDemo/static/js/main.ff81b456.chunk.js"
+  },
+  {
+    "revision": "817f8a82ebc1c053406d",
+    "url": "/bonjourDemo/static/js/runtime-main.914a0256.js"
+  },
+  {
+    "revision": "61bbf12ae90a30cd432d77087921a02f",
+    "url": "/bonjourDemo/static/media/nat-4.61bbf12a.jpg"
+  },
+  {
+    "revision": "197f70f3dc27acb6c2fe4a66f7675982",
+    "url": "/bonjourDemo/static/media/travel-1.197f70f3.jpeg"
+  },
+  {
+    "revision": "9d7e5bf9988691674214ced016e1aa63",
+    "url": "/bonjourDemo/static/media/travel-2.9d7e5bf9.jpeg"
+  },
+  {
+    "revision": "a742340329f662cbc03a6908e9762fda",
+    "url": "/bonjourDemo/static/media/travel-3.a7423403.jpeg"
+  },
+  {
+    "revision": "45deb3c1b67561e17c324fba7c21f700",
+    "url": "/bonjourDemo/static/media/travel-4.45deb3c1.jpeg"
+  },
+  {
+    "revision": "aa69fcd068decd5e8d9cf0e78bc4fa4e",
+    "url": "/bonjourDemo/static/media/travel-5.aa69fcd0.jpeg"
+  }
+]);
